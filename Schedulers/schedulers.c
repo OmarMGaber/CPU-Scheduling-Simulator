55fd281f9d426f78956468d311aeff97bf4254a3
OmarMGaber <omarmgaber37@gmail.com>
2023-12-02 13:44:10 +0200

    Added new schedulers

--- Schedulers/schedulers.c
+++ Schedulers/schedulers.c
@@ -2,11 +2,34 @@
 // Created by Omar on 01/12/2023.
 //
 
-
+#include "schedulers.h"
 #include "../Models/process.h"
 
-Process *firstComeFirstServe(NewProcess *newProcess, int numOfProcesses) {
-//    UnitTime currentTime = (neglectContextSwitching) ? newProcess[0].arrivalTime: 0;
+
+Process *firstComeFirstServe(NewProcess *newProcess, int numOfProcesses, UnitTime *waitTime) {
+    UnitTime currentTime = (neglectContextSwitching) ? newProcess[0].arrivalTime : 0;
+    *waitTime = (neglectContextSwitching) ? 0 : 1;
+
+    Process *processes = malloc(sizeof(Process) * numOfProcesses);
+
+    for (int i = 0; i < numOfProcesses; i++) {
+        processes[i].arrivalTime = newProcess[i].arrivalTime;
+        processes[i].processID = newProcess[i].processID;
+        processes[i].burstTime = newProcess[i].burstTime;
+
+        processes[i].responseTime = currentTime;
+        processes[i].waitingTime = currentTime;
+
+        currentTime += processes[i].burstTime;
+        processes[i].turnaroundTime = currentTime;
+        processes[i].completionTime = currentTime;
+        waitTime += processes[i].waitingTime;
+    }
+
+    return processes;
+}
+
+Process *shortestJobFirst(NewProcess *newProcess, int numOfProcesses) {
     Process *processes = malloc(sizeof(Process) * numOfProcesses);
     UnitTime currentTime = 0;
     UnitTime waitTime = 0;
@@ -26,4 +49,3 @@ Process *firstComeFirstServe(NewProcess *newProcess, int numOfProcesses) {
 
     return processes;
 }
-
--- Schedulers/schedulers.h
+++ Schedulers/schedulers.h
@@ -9,13 +9,28 @@
 #include <malloc.h>
 #include "../Models/process.h"
 
-#define bool int
-#define true 1
-#define false 0
+#define bool short
+#define true (short)1
+#define false (short)0
 
 static bool neglectContextSwitching = true;
 
+Process *firstComeFirstServe(NewProcess *newProcess, int numOfProcesses, UnitTime *waitTime);
 
-Process *firstComeFirstServe(NewProcess *newProcess, int numOfProcesses);
+Process *shortestJobFirst(NewProcess *newProcess, int numOfProcesses);
+
+Process *shortestRemainingTimeFirst(NewProcess *newProcess, int numOfProcesses);
+
+Process *roundRobin(NewProcess *newProcess, int numOfProcesses, UnitTime quantum);
+
+Process *highestResponseRatioNext(NewProcess *newProcess, int numOfProcesses);
+
+Process *longestJobFirst(NewProcess *newProcess, int numOfProcesses);
+
+Process *multilevelQueueScheduling(NewProcess *newProcess, int numOfProcesses);
+
+Process *multilevelFeedbackQueueScheduling(NewProcess *newProcess, int numOfProcesses);
+
+Process *priorityScheduling(NewProcess *newProcess, int numOfProcesses);
 
 #endif //CPU_SCHEDULING_SIMULATOR_SCHEDULERS_H
